09b6587ce15ca1995c6c1fa628e6c6fb33b2162e
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-02-16 15:19:20 +0100

    Es 2/B

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,10 @@ const number = 100 ; /* number */
 
 console.log(number);
 
-const PI = 3.14 ; /* number */
+let PI = 3.14 ; /* number */
 
 console.log(PI)
+
+PI = 3.1416 ; /* number */
+
+console.log(PI) /* checking PI change */
